0c4fe4235f151f9507baf338569e4a264a22ad3e
Uladzislau <uadzislau.shvorab@fortenext.com>
2025-08-19 14:46:15 +0300

    feat(icons)

--- force-app/main/default/lwc/icon/icon.js
+++ force-app/main/default/lwc/icon/icon.js
@@ -1,5 +1,10 @@
 import { LightningElement, api } from "lwc";
+import file from "./templates/file.html";
 import bannerArrow from "./templates/bannerArrow.html";
+import facebook from "./templates/facebook.html";
+import linkedin from "./templates/linkedin.html";
+import instagram from "./templates/instagram.html";
+import youtube from "./templates/youtube.html";
 
 export default class Icon extends LightningElement {
     @api type;
@@ -8,6 +13,17 @@ export default class Icon extends LightningElement {
         switch (this.type) {
             case "banner-arrow":
                 return bannerArrow;
+            case "file":
+                return file;
+
+            case "facebook":
+                return facebook;
+            case "instagram":
+                return instagram;
+            case "linkedin":
+                return linkedin;
+            case "youtube":
+                return youtube;
             default:
                 return super.render();
         }
